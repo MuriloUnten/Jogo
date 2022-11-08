f23ca6adb5e1b29f5dc43955b8a8077282c610b8
MuriloUnten <murilokenjiunten@gmail.com>
2022-11-08 11:35:14 -0300

    Created class Obstacle

---
+++ src/Obstacle.cpp
@@ -0,0 +1,47 @@
+#include "Obstacle.hpp"
+
+namespace Entities
+{
+    namespace StaticEntities
+    {
+        Obstacle::Obstacle(const std::string fileName, sf::Vector2f size, sf::Vector2f position, int newDamage, int newSlow):
+        Entity(fileName, size, position)
+        {
+            damage = newDamage;
+            slow = newSlow;
+        }
+
+
+        Obstacle::Obstacle():
+        Entity()
+        {
+            damage = 0;
+            slow = 0;
+        }
+
+
+        void Obstacle::setDamage(const int newDamage)
+        {
+            damage = newDamage;
+        }
+
+
+        const int Obstacle::getDamage() const
+        {
+            return damage;
+        }
+
+
+        void Obstacle::setSlow(const int newSlow)
+        {
+            slow = newSlow;
+        }
+
+
+        const int Obstacle::getSlow() const
+        {
+            return slow;
+        }
+
+    }// namespace Staticentities
+}// namespace Entities
---
+++ src/Obstacle.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Entity.hpp"
+
+namespace Entities
+{
+    namespace StaticEntities
+    {
+        class Obstacle : public Entity
+        {
+        protected:
+            int damage;
+            int slow;
+
+        public:
+            Obstacle(const std::string fileName, sf::Vector2f size, sf::Vector2f position=sf::Vector2f(0.0f,0.0f), int newDamage=0, int newSlow=0);
+            Obstacle();
+            ~Obstacle();
+
+            void setDamage(const int newDamage);
+            const int getDamage() const;
+
+            void setSlow(const int newSlow);
+            const int getSlow() const;
+        };
+
+    }// namespace StaticEntities
+}// namespace Entities
